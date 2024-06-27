c68c6bda5e8b09ceb07100d0ed18c5137d42d941
Adetunji-Philip240 <adewalegithub240@gmail.com>
2024-06-27 05:52:06 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello World</h1>
+        <p>We are learning how to install and run react js project</p>
+        <p>Hi</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
